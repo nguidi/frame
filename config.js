a9092d31fff93880a50fbc8467ba8c3e767e93b4
Luis Fabio <luis.fabio.1990@gmail.com>
2014-05-07 15:59:26 -0300

    pruebas

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
-require
-	.config(
-		{
-			map:
-			{
-				'*':
-				{
-					'css':	'bower_components/require-css/css'
-				} 
-			}
-		,   paths:
-			{
-				jquery:		'bower_components/jquery/dist/jquery.min'
-			,	can:		'bower_components/canjs/amd/can'
-			,	bootstrap:	'bower_components/bootstrap/dist'
-			,	fontawesome:'bower_components/fontawesome'
-			,	localstorage:'bower_components/canjs-localstorage/can.localstorage'
-			}
+require.config( {
+	map:{
+		'*':{
+			'css':	'bower_components/require-css/css'
+		} 
+	}
+	,   paths:{
+			jquery:		'bower_components/jquery/dist/jquery.min'
+		,	can:		'bower_components/canjs/amd/can'
+		,	bootstrap:	'bower_components/bootstrap/dist'
+		,	fontawesome:'bower_components/fontawesome'
+		,	localstorage:'bower_components/canjs-localstorage/can.localstorage'
 		}
-	)
+	}
+)
--- controls/table/table.js
+++ controls/table/table.js
@@ -2,27 +2,62 @@ define(
 	[
 		'lib/util'
 	]
-,	function()
-	{
-		can.Control(
-			'TableControl'
-		,	{
-				defaults:
-				{
-					view: false
+,	function(){
+		can.Control('TableControl'
+			,{
+				defaults:{
+					view_filter: false
+				,	view_table: false
 				,	data: []
 				}
-			}
-		,	{
-				init: function(element,options)
-				{
+			
+			},{
+				
+				init: function(element,options){
+
 					can.append(
-						element
+						this.element
 					,	can.view(
-							options.view
-						,	options.data
+							this.options.view_filter
 						)
 					)
+					
+					if (typeof options.data == 'object')
+						this.agregarDatos(options.data)
+					else
+						if (can.isDeferred(options.data()))
+							options.data().then(can.proxy(this.agregarDatos,this))
+						else
+							if (options.data() instanceof can.Model)
+								options.data.aDeferred().then(can.proxy(this.agregarDatos,this))
+
+					this.configTable()
+				
+				}
+				
+			,	configTable: function(){
+					var $table = this.element.find('table')
+
+					$table.addClass(this.options.class)
+					$table.addClass(this.options.type)
+					if (this.options.id)
+						$table.attr('id', this.options,id)
+				}
+
+			,	agregarDatos: function(data){
+					can.append(
+						this.element,
+						can.view(
+							this.options.view_table 
+						,	data
+						)
+					)
+				}
+
+			,	'.filter click': function(el, ev) {
+					can.$('body').find('#table1').remove()
+
+					this.options.data.filter(2).then(can.proxy(this.agregarDatos,this))
 				}
 			}
 		)
