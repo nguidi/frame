645a762e4dfb964f2f5c35bfaecbcb82bd9b8cd8
Luis <luis.fabio.1990@gmail.com>
2014-05-26 14:15:04 -0300

    .

--- controls/wizard/wizard.js
+++ controls/wizard/wizard.js
@@ -2,6 +2,7 @@ define(
 	[
 	'lib/util'
 ,	'lib/styles'
+,	'css!controls/wizard/wizard'
 	]
 ,	function()
 	{
@@ -18,13 +19,108 @@ define(
 		,	{
 
 				init: function (element, options){
+					can.append( this.element, 
+						can.view(
+							this.options.view))
 					
+
 					this.wizardLIFO = new can.List([])
 
-					this.loadStep()
+					this.wizardData = new can.Map({})
+
+					this.wizardLIFO.bind(
+						'add'
+					,	can.proxy(this._render_next_step,this)
+					)
+
+					this.wizardLIFO.bind(
+						'remove'
+					,	can.proxy(this._render_prev_step,this)
+					)
+					var primerPaso = this.findPrimerPaso()
+
+					this.wizardLIFO.push(primerPaso)
+				}
+
+			, 	findPrimerPaso: function(){
+					var initialSteps = can.grep(this.options.steps, function(step, i){ return step.initial })
+					return initialSteps.shift()
+				}
+
+			, 	_render_next_step: function(event, steps, index){
+						
+						var step = steps.shift()
+
+						var $li = can.$('<li>')
+						$li.addClass('active')
+						var $a = can.$('<a>')
+						$a.html(step.title)
+
+						$li.append($a)
+						
+						this.element.find('.nav-wizard li.active').removeClass('active')
+
+						$li.insertBefore(
+							this.element.find('.nav-wizard li:last')
+							)
+
+						this.generarContenido(step)
+
+						this.wizardData.attr(step.key, {})
+
+
+
+				}
+			, 	_render_prev_step: function(event, steps, index){
+
+					var prevElem = this.element.find('.nav-wizard li.active').prev()
+					prevElem.addClass('active')
+
+					prevElem.next().remove()
+
+					var step = this.wizardLIFO.attr(index -1)
+					this.generarContenido(step)
 
 				}
 
+			,	generarContenido: function(step) {
+
+						var $content = this.element.find('.panel-body').empty()
+
+						var $div2 = can.$('<div>')
+						new step.control($div2, {}, {})
+
+						$div2.appendTo($content)
+
+						this.element.find('.btnnext').attr('disabled', true)
+						
+						if (step.initial) {
+							this.element.find('.btnback').attr('disabled', true)
+						} else {
+							this.element.find('.btnback').attr('disabled', false)	
+						}
+
+						if (step.final){
+							this.element.find('.btnnext').html('finalizar')
+						}
+
+			}
+			,	'frame.wizard.next': function(el,ev,options)
+				{
+					this.options.nextStep = can.grep(this.options.steps, function(step, i){ return step.key ==  stepKey}).shift()
+				
+					this.element.find('.btnnext').attr('disabled',false)
+				}
+
+			,	'.btnnext click': function(el,ev){
+					this.wizardLIFO.push(this.options.nextStep)
+				}
+				
+			,	'.btnback click': function(el,ev){
+					this.wizardLIFO.pop()
+				}
+
+				/*
 
 			,	'.btnstep click': function(el,ev){
 					
@@ -53,6 +149,14 @@ define(
 
 					can.$('.step').remove()
 
+					this.element.find('.nav-wizard')
+						.prepend(
+								can.$('<li>')
+							)
+						var $li = can.$('<li>')
+						$li.addClass('active')
+						var $a = can.$('<a>')
+						$a.html
 					can.append(
 						this.element
 					,	can.view(
@@ -60,14 +164,6 @@ define(
 						,	this.options.data
 						)
 					)
-
-					if ( this.options.initial == true ) {
-						$('.btnback').hide()
-					} else {
-						$('.btnback').show()	
-					}
-
-					$('.btnnext').hide()
 				}
 
 			, 	'frame.step.b': function(){
@@ -84,6 +180,7 @@ define(
 					$('.btnnext').show()
 
 				}
+				*/
 			}
 		)	
 	}
